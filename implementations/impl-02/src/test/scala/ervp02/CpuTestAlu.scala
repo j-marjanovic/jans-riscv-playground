99f08fb87d49570b4d55c71433171d45faeca1da
Jan Marjanovic <jan.marjanovic@outlook.com>
2021-07-23 10:31:46 +0200

    ervp02: reorganize tests

---
+++ implementations/impl-02/src/test/scala/ervp02/CpuTestAlu.scala
@@ -0,0 +1,35 @@
+// Copyright (c) 2021 Jan Marjanovic
+// This code is licensed under a 3-clause BSD license - see LICENSE.txt
+
+package ervp02
+
+class CpuTestAlu(c: Cpu) extends CpuTestGeneric(c) {
+
+  override val INSTRS: String =
+    """ 200:	00100513          	li	a0,1
+      | 204:	00200593          	li	a1,2
+      | 208:	00b50633          	add	a2,a0,a1
+      | 20c:	00a60613          	addi	a2,a2,10
+      | 210:	000022b7          	lui	t0,0x2
+      | 214:	10028293          	addi	t0,t0,256 # 2100 <_end+0x1ecc>
+      | 218:	00c2a823          	sw	a2,16(t0)
+      | 21c:	fea28fa3          	sb	a0,-1(t0)
+      | 220:	feb28f23          	sb	a1,-2(t0)
+      |""".stripMargin
+
+  override def final_check(): Unit = {
+    val mem_txs = mem_dummy.mem_txs.toList
+    expect(mem_txs(0).addr == 0x110, "mem addr 0")
+    expect(mem_txs(0).data == 0xd, "mem data 0")
+
+    expect(mem_txs(1).addr == 0xff, "mem addr 1")
+    expect(mem_txs(1).data == 0x1, "mem data 1")
+    // TODO: add byte enable
+
+    expect(mem_txs(2).addr == 0xfe, "mem addr 2")
+    expect(mem_txs(2).data == 0x2, "mem data 2")
+    // TODO: add byte enable
+  }
+
+  run_test()
+}
---
+++ implementations/impl-02/src/test/scala/ervp02/CpuTestBranch.scala
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 Jan Marjanovic
+// This code is licensed under a 3-clause BSD license - see LICENSE.txt
+
+package ervp02
+
+class CpuTestBranch(c: Cpu) extends CpuTestGeneric(c) {
+  override val INSTRS: String =
+    """ 00000200 <_start>:
+      | 200:	00100293          	li	t0,1
+      | 204:	00200313          	li	t1,2
+      | 208:	00300393          	li	t2,3
+      | 20c:	00628c63          	beq	t0,t1,224 <set_to_3>
+      | 210:	00731663          	bne	t1,t2,21c <set_to_2>
+      |
+      |00000214 <set_to_1>:
+      | 214:	00100513          	li	a0,1
+      | 218:	0100006f          	j	228 <write_data>
+      |
+      |0000021c <set_to_2>:
+      | 21c:	00200513          	li	a0,2
+      | 220:	0080006f          	j	228 <write_data>
+      |
+      |00000224 <set_to_3>:
+      | 224:	00300513          	li	a0,3
+      |
+      |00000228 <write_data>:
+      | 228:	000022b7          	lui	t0,0x2
+      | 22c:	00a2a023          	sw	a0,0(t0) # 2000 <_end+0x1dbc>
+      |""".stripMargin
+
+  override def final_check(): Unit = {
+    val mem_txs = mem_dummy.mem_txs.toList
+    expect(mem_txs.head.addr == 0, "mem addr 0")
+    expect(mem_txs.head.data == 0x2, "mem data 0")
+  }
+
+  run_test()
+}
